6291904e6867a63b9b2a260e2f605937640b1aed
Mike M <mikemobilio@outlook.com>
2012-10-30 23:59:47 -0300

    Update skype.php
    showing usage.

--- skype.php
+++ skype.php
@@ -1,4 +1,5 @@
 <?php
+	// Usage: skype.php?user=<skypename>
 	// (location)/Skype Resolve.exe <skype username> <wait (ms)> [<directory>]
 	$result = shell_exec('"Skype Resolve.exe" ' . $_GET['user'] . ' 2000');
 	
